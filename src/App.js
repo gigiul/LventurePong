bab835ec8e4be67765c18afb12daaf29954832a3
luigi <luigi@luigi.ubuntu>
2021-12-26 17:15:06 +0100

    compiling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import logo from './pong.png';
 import './App.css';
+import Button from "@mui/material/Button"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Button variant='contained' color="success">Login</Button>
+    	<p>
+        Join Lventure ping-pong community!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
-  );
+    </div>     
+);
 }
 
 export default App;
